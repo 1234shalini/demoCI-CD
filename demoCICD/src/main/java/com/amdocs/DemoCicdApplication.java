2922733285ba3d9ab5684197198373dcf8ec341a
SHALINI <iamshalini03@gmail.com>
2023-10-20 11:46:06 +0530

    firstcommit

---
+++ demoCICD/src/main/java/com/amdocs/DemoCicdApplication.java
@@ -0,0 +1,15 @@
+package com.amdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdApplication.class, args);
+		System.out.println("Hello World");
+		
+	}
+
+}
